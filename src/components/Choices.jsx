c69b9697f275729c975898d90d60422fbc988ca5
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-01-29 14:36:51 +0700

    import action

--- src/components/Choices.jsx
+++ src/components/Choices.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { toggleChoices } from "../actions/gameAction";
 
 export class Choices extends Component {
   handleToggle = id => {
